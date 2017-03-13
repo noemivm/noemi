74e3cc18e5550e8e5c1a9e58f6541a3fa6f05b11
Profesor <Profesor@PCDAM114>
2017-03-13 09:22:00 +0100

    Borrados los comentarios

--- src/archivoprimero/ArchivoPrimero.java
+++ src/archivoprimero/ArchivoPrimero.java
@@ -1,23 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package archivoprimero;
 
-/**
- *
- * @author Profesor
- */
 public class ArchivoPrimero {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Inicio del proyecto");
         System.out.println("Inicio del proyecto 2");
-        System.out.println("Inicio del proyecto 3");
+        System.out.println("Inicio del proyecto 4");
     }
     
 }
